bd1493db0e5b3891323384d1a8f99dca1970296f
Sihuan Newrise <sihuannewrise@gmail.com>
2023-03-09 21:44:54 +0300

    json

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import os
-from datetime import datetime
+import json
+
+# from datetime import datetime
 from openpyxl import load_workbook
-from app.core.schemas.contract import ContractCreate
 
 CWD = os.getcwd()
 
@@ -14,30 +15,46 @@ DESTIN_FILE_SUFFIX = ' - out'
 DESTIN_SHEET_NAME = 'proc'
 
 file = SOURCE_DIR + SOURCE_FILE_NAME
+
 PERIOD, VAT, DATA, four, five, six, AMOUNT = range(7)
 
-ca = set()
-contract = set()
+
+class Expenses(dict):
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
 
 
-async def create_new_expense(filename):
+def create_dict(filename):
     """Загружаем файл.
     """
     wb = load_workbook(filename=filename)
     ws = wb.active
+    expenses = Expenses()
 
-    for row in ws.iter_rows(min_row=2, max_row=3, values_only=True):
-        current_date = datetime.strptime(row[PERIOD], '%d.%m.%Y').date()
+    for row in ws.iter_rows(min_row=2, values_only=True):
+        # current_date = datetime.strptime(row[PERIOD], '%d.%m.%Y').date()
         # year = current_date.strftime('%Y')
-        # current_month = current_date.strftime('%m')
+        # current_month = int(current_date.strftime('%m'))
         _, current_ca, current_contract, *_ = row[DATA].split('\n')
-        current_sum = round(float(row[AMOUNT]), 2)
+        current_sum = int(row[AMOUNT])
+
+        # по годам
+        if current_contract not in expenses[current_ca]:
+            expenses[current_ca][current_contract] = current_sum
+        else:
+            expenses[current_ca][current_contract] += current_sum
+
+        # с разбивкой по месяцам
+        # if current_month not in expenses[current_ca][current_contract]:
+        #     expenses[current_ca][current_contract][current_month] = round(
+        #         current_sum/1.2, 2)
+        # else:
+        #     expenses[current_ca][current_contract][current_month] += round(
+        #         current_sum/1.2, 2)
+    return expenses
 
 
-        expense = {
-            'period': current_date,
-            'ca_id': current_ca,
-            'contract_id': current_contract,
-            'amount': current_sum,
-        }
-    return None
+dict = create_dict(file)
+with open("files/expenses.json", "w", encoding='UTF-8') as outfile:
+    json.dump(dict, outfile, ensure_ascii=False)
